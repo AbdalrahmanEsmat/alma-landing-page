4fbf3b4aa1c9e7393aff7231bf681b07992f2a4f
abdalrahman-2 <abdalrahmanesmat@outlook.com>
2026-08-01 14:44:34 +0300

    adding the translation to the home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+import { getTranslations, setRequestLocale } from 'next-intl/server';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
+  const translation = await getTranslations('Home');
+
   return (
     <main>
-      <h1>الصفحة الرئسيسية</h1>
+      <h1>{translation('title')}</h1>
     </main>
   );
 }
